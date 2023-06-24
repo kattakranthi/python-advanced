c996b0b750f9d1fdd2787fe9e65369ddaa910778
kattakranthi <37553537+kattakranthi@users.noreply.github.com>
2023-06-24 09:09:50 -0400

    Create sparkexamples.py

---
+++ sparkexamples.py
@@ -0,0 +1,6 @@
+//spark load csv files and save in parquet.
+//spark load txt files and save in orc format. 
+//orc vs parquet
+//spark use joins of 2 dataframes, map employee and department tables
+//aggregrate functions 
+//Add total - of department salary, show the data by department salary, show the employee count by Organization.
