4692bcb1b96598e7118d53ffb2dc1e0775246a6f
kattakranthi <37553537+kattakranthi@users.noreply.github.com>
2023-06-24 09:08:26 -0400

    Update pandasloaddifferentformats.py

--- pandasloaddifferentformats.py
+++ pandasloaddifferentformats.py
@@ -1,1 +1,5 @@
+//pandas load csv files and save in parquet.
+//pandas load txt files and save in orc format. 
+//orc vs parquet
+//pandas use joins of 2 dataframes, map employee and department tables
 
