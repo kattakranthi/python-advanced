ed0c9fe68a8d15a114e15d69ca77fdeb6d5bae47
kattakranthi <37553537+kattakranthi@users.noreply.github.com>
2023-07-01 16:15:11 -0400

    Update pandasloaddifferentformats.py

--- pandasloaddifferentformats.py
+++ pandasloaddifferentformats.py
@@ -2,4 +2,18 @@
 //pandas load txt files and save in orc format. 
 //orc vs parquet
 //pandas use joins of 2 dataframes, map employee and department tables
+import csv
 
+filename = "data.csv"  # Replace with the path to your CSV file
+
+with open(filename, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
+import pandas as pd
+
+filename = "data.csv"  # Replace with the path to your CSV file
+//Read the csv file to a dataframe.
+data = pd.read_csv(filename)
+print(data)
