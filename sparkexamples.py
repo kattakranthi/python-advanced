340b5b0f0a39f355ef68c98331f7f24573f0768a
kattakranthi <37553537+kattakranthi@users.noreply.github.com>
2023-07-30 09:52:06 -0400

    Update sparkexamples.py

--- sparkexamples.py
+++ sparkexamples.py
@@ -4,3 +4,24 @@
 //spark use joins of 2 dataframes, map employee and department tables
 //aggregrate functions 
 //Add total - of department salary, show the data by department salary, show the employee count by Organization.
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+# Create a SparkSession
+spark = SparkSession.builder.appName("Employee CSV to Parquet").getOrCreate()
+
+# Load the CSV file into a DataFrame
+csv_file_path = "employees.csv"
+df = spark.read.csv(csv_file_path, header=True, inferSchema=True)
+
+# Add one more column to the DataFrame
+df_with_salary_multiplier = df.withColumn("SalaryMultiplier", col("Salary") * 2)  # You can adjust the multiplier as needed
+
+# Save the DataFrame as a Parquet file
+parquet_file_path = "output/"
+df_with_salary_multiplier.write.parquet(parquet_file_path)
+
+# Stop the SparkSession
+spark.stop()
+
