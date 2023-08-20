b9e349125bbfa80807d5d7fd31d20d81c116d51c
kattakranthi <37553537+kattakranthi@users.noreply.github.com>
2023-08-20 08:42:37 -0400

    Update pandasloaddifferentformats.py

--- pandasloaddifferentformats.py
+++ pandasloaddifferentformats.py
@@ -17,3 +17,22 @@ filename = "data.csv"  # Replace with the path to your CSV file
 //Read the csv file to a dataframe.
 data = pd.read_csv(filename)
 print(data)
+
+#create a dataframe from excel
+df = pd.read_excel('data.xlsx', sheet_name='Sheet1')
+
+print(df)
+
+#create a dataframe from database
+import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+
+query = 'SELECT * FROM people'
+df = pd.read_sql(query, conn)
+
+conn.close()
+
+print(df)
+
